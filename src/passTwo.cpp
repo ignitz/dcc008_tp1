598794530980449d6b5a9b808c0bbf8899eac8f1
ignitz <ignitzhjfk@gmail.com>
2016-09-15 15:13:28 -0300

    To Merge Master

--- src/passTwo.cpp
+++ src/passTwo.cpp
@@ -15,53 +15,37 @@ pass_two(std::ifstream& file, std::fstream& objeto, SymbolTable& table_symbol, b
 	file.seekg(0, file.beg); // Posiciona no inicio do arquivo novamente
 
   TableOpcode table_opcode;
-  String line, output, fields, opcode;  // campos da instrução
+  String line, output, opcode;  // campos da instrução
+  vector<string> fields;        // tokens
 
   output += HEAD_MIF;
 
   while (getline(file, line)) {
-    if (bVerbose) cout << "DEBUG: " << line << endl;
     if (clearLine(line))
 			continue;           // Se a linha não contiver instruções
 
-    // boost::split(fields, line, // Separa a linha em tokens
-  	// 		boost::is_any_of("\t "), boost::token_compress_on);
+    if (bVerbose) cout << "DEBUG: " << line << endl;
+
+    boost::split(fields, line, // Separa a linha em tokens
+          boost::is_any_of("\t "), boost::token_compress_on);
+
+    if (fields[0].back() == ':') {
+      if (fields[0].front() == '_') {
+        fields.erase(fields.begin()); // Elimina label
+      }
+      // TODO
+      // Tratamento das variáveis
+
+    }
+
+    // table_opcode.get_opcode_value
+
 
   }
 
-  output += END_MIF;
-  objeto << output;
-
   if (bVerbose) table_symbol.printSymbols();
 
-	// int location_counter, length, type; // variáveis diversas
-	// final int END_STATEMENT = −2; // sinaliza final da entrada
-	// final int MAX_CODE = 16; // máximo de bytes de código por instrução
-	// byte code[ ] = new byte[MAX_CODE]; //contém código gerado por instrução
-  //
-  // location_counter = 0; // monta a primeira instrução em 0
-  //
-	// while (more_input) { // more_input ajustada para falso por END
-	// 	type = read_type( );      // obtém campo de tipo da próxima linha
-	// 	opcode = read_opcode( );  // obtém campo de opcode da próxima linha
-	// 	length = read_length( );  // obtém comprimento de campo da próxima linha
-	// 	line = read_line( );      // obtém a linha de entrada propriamente dita
-  //
-	// 	if (type != 0) { // tipo 0 é para linhas de comentário
-	// 		switch(type) { // gerar o código de saída
-	// 			case 1: eval_type1(opcode, length, line, code); break;
-	// 			case 2: eval_type2(opcode, length, line, code); break;
-	// 		 // outros casos aqui
-	// 		}
-	// 	}
-	// 	write_output(code); // escreva o código binário
-	// 	write_listing(code, line); // imprima uma linha na listagem
-	// 	 location_counter = location_counter + length; // atualize loc_ctr
-	// 	 if (type == END_STATEMENT) { // terminamos a entrada?
-	// 	 	more_input = false; // se terminamos, execute tarefas de manutenção
-	// 	 	finish_up( ); // execute tarefas de manutenção gerais e termine
-	// 	 }
-	// 	}
-	// }
-
+  // Finaliza gravando em arquivo
+  output += END_MIF;
+  objeto << output;
 } // end passtwo
